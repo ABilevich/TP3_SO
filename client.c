bbfe9ff573dae18e41b046d12875a52b0a3b9356
AndyBilevich <abilevich@itba.edu.ar>
2020-05-28 14:19:29 -0300

    chalenge2

--- client.c
+++ client.c
@@ -1,9 +1,67 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
 
 #include "client.h"
 
-int main(void)
+int main(int argc, char const *argv[])
 {
-    printf("a laburar\n");
+    int sock = connectToServer();
+    chalenge1(sock);
+    chalenge2(sock);
+    while (1)
+    {
+    }
+    //asd
+    return 0;
+}
+
+int connectToServer()
+{
+    int sock = 0;
+    struct sockaddr_in serv_addr;
+
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        printf("\n Socket creation error \n");
+        return -1;
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    // Convert IPv4 and IPv6 addresses from text to binary form
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0)
+    {
+        printf("\nInvalid address/ Address not supported \n");
+        return -1;
+    }
+
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    {
+        printf("\nConnection Failed \n");
+        return -1;
+    }
+
+    return sock;
+}
+
+void chalenge1(int sock)
+{
+    char *hello = "entendido\n";
+    send(sock, hello, strlen(hello), 0);
+}
+
+void chalenge2(int sock)
+{
+    char *msg0 = "# 75 72 72 75 80 77 80 75 *\n";
+    char *msg1 = "35 75 72 72 75 80 77 80 75 42\n";
+    char *msg2 = "75 72 72 75 80 77 80 75\n";
+    char *msg3 = "#\n";
+    char *msg4 = "VALGRIND_ES_GENIAL\n";
+    char *msg5 = "# left left *\n";
+    char *msg6 = "#flag*\n";
+    send(sock, msg6, strlen(msg6), 0);
 }
---
+++ client.h
@@ -0,0 +1,9 @@
+#define PORT 8080
+#define left '75'
+#define right '77'
+#define up '72'
+#define down '80'
+
+int connectToServer();
+void chalenge1(int sock);
+void chalenge2(int sock);
